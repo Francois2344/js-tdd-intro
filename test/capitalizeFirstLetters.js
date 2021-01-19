cec93c0515b7cda4e39df3b16d8c15de80dec0ee
Francois2344 <67748446+Francois2344@users.noreply.github.com>
2021-01-19 10:18:10 +0100

    write first test

---
+++ test/capitalizeFirstLetters.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+
+assert.strictEqual(typeof capitalizeFirstLetters, 'function');
+assert.strictEqual(capitalizeFirstLetters.length, 1);
+assert.strictEqual(capitalizeFirstLetters(' i am learning TDD'), 'I Am Learning TDD');
+assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
+assert.strictEqual(capitalizeFirstLetters(''), '');
