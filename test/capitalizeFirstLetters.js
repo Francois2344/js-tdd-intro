acb4bab3a8a30bf7b4ee59436f83ed2b75230dbc
Francois2344 <67748446+Francois2344@users.noreply.github.com>
2021-01-19 14:00:17 +0100

    git tag step 1

--- test/capitalizeFirstLetters.js
+++ test/capitalizeFirstLetters.js
@@ -1,8 +1,16 @@
-const assert = require('assert');
+const assert = require("assert");
 
+function capitalizeFirstLetters(input) {
+    const words = input.split(' ');
+    for (i=0; i<words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1);
+    }
+    return words.join(' ');
+  }
 
-assert.strictEqual(typeof capitalizeFirstLetters, 'function');
+assert.strictEqual(typeof capitalizeFirstLetters,'function');
 assert.strictEqual(capitalizeFirstLetters.length, 1);
-assert.strictEqual(capitalizeFirstLetters(' i am learning TDD'), 'I Am Learning TDD');
-assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
-assert.strictEqual(capitalizeFirstLetters(''), '');
+assert.strictEqual(capitalizeFirstLetters('i am learning TDD'),
+  'I Am Learning TDD');
+assert.strictEqual(capitalizeFirstLetters("z"), "Z");
+assert.strictEqual(capitalizeFirstLetters(""), "");
